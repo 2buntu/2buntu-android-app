998f4ff27c4a2e6c819420410e33caba48041545
Nathan Osman <admin@quickmediasolutions.com>
2013-06-15 12:49:32 -0700

    Finished customizing style of action bar.

--- src/com/twobuntu/twobuntu/ArticleDetailFragment.java
+++ src/com/twobuntu/twobuntu/ArticleDetailFragment.java
@@ -18,6 +18,22 @@ public class ArticleDetailFragment extends Fragment {
 	// This is the ID of the article being displayed.
 	public static final String ARG_ARTICLE_ID = "article_id";
 	
+	// Generates the HTML for the entire page given the title and body to display.
+	private String generateHTML(String title, String author, String body) {
+	    return "<html>" +
+	           "<head>" +
+	           "  <link rel='stylesheet' href='css/style.css'>" +
+	           "</head>" +
+	           "<body>" +
+	           "<header>" +
+	             "<h2>" + title + "</h2>" +
+	             "<p>by " + author + "</p>" +
+	           "</header>" +
+	           body +
+	           "</body>" +
+	           "</html>";
+	}
+	
 	// Begins loading the article and preparing it for display.
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,12 +47,13 @@ public class ArticleDetailFragment extends Fragment {
 		    Uri uri = Uri.withAppendedPath(ArticleProvider.CONTENT_LOOKUP_URI,
 		    		String.valueOf(getArguments().getLong(ARG_ARTICLE_ID)));
 		    Cursor cursor = getActivity().getContentResolver().query(uri,
-		    		new String[] { Articles.COLUMN_TITLE, Articles.COLUMN_BODY }, null, null, null);
+		    		new String[] { Articles.COLUMN_TITLE, Articles.COLUMN_AUTHOR_NAME,
+		    		Articles.COLUMN_BODY }, null, null, null);
 		    cursor.moveToFirst();
 		    // Set the title and body.
-		    getActivity().setTitle(cursor.getString(0));
 			WebView webView = (WebView)rootView.findViewById(R.id.article_content);
-			webView.loadData(cursor.getString(1), "text/html", "utf-8");
+			webView.loadDataWithBaseURL("file:///android_asset/", generateHTML(cursor.getString(0),
+			        cursor.getString(1), cursor.getString(2)), "text/html", "utf-8", null);
 		}
 		return rootView;
 	}

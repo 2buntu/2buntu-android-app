a941c04b22e16d8cb7cec0966107f270507525ac
Nathan Osman <admin@quickmediasolutions.com>
2013-11-03 18:52:51 -0800

    Renamed ArticleListActivity to MainActivity.

---
+++ src/com/twobuntu/ui/MainActivity.java
@@ -0,0 +1,54 @@
+package com.twobuntu.ui;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.twobuntu.twobuntu.R;
+
+public class MainActivity extends FragmentActivity {
+	
+	ActionBarDrawerToggle mDrawerToggle;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		String[] navigationTitles = getResources().getStringArray(R.array.navigation);
+		DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		ListView drawerList = (ListView)findViewById(R.id.left_drawer);
+		
+		drawerList.setAdapter(new ArrayAdapter<String>(
+				this,
+				android.R.layout.simple_list_item_1,
+				navigationTitles));
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        
+        mDrawerToggle = new ActionBarDrawerToggle(
+        		this,
+        		drawerLayout,
+        		R.drawable.ic_drawer,
+        		R.string.drawer_open,
+        		R.string.drawer_close);
+        drawerLayout.setDrawerListener(mDrawerToggle);
+	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+	
+	@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
